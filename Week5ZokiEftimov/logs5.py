2179ae84eb377524534a58667b37d428857da52d
Zoki-Eftimov <zkeftimov@gmail.com>
2023-05-18 20:37:14 -0700

    logs 5.py

--- Week5ZokiEftimov/logs5.py
+++ Week5ZokiEftimov/logs5.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-
 #Zoran Eftimov
 #prof. Ski Kacoroski
 #SEC444 - Automation/Configuration & Management
-#4-27-2023
+#5-17-2023
 
-# Description
+# Description - This program scans the wafeventlog file and creates a redirects.csv file that counts the numbers of times servers are redirected.
 
 # Versioning
-# Zoki: initial version
+# Zoki: initial version v0.1
 
 # Set up initial variables and imports
-
 import re
 from collections import defaultdict
 import csv
@@ -28,7 +26,7 @@ for line in lines:
     if match:
         from_url = match.group(1)
         to_url = match.group(2)
-        
+
         # Ignore redirects containing "Basic" or "ActiveSync"
         if "Basic" not in from_url and "ActiveSync" not in from_url:
             redirect_key = (from_url, to_url)
@@ -42,8 +40,4 @@ with open('redirects.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Count', 'From', 'To'])
     writer.writerows([(count, from_url, to_url) for (from_url, to_url), count in sorted_redirects])
-# Main routine that is called when script is run
 
-# Run main() if script called directly, else use as a library to be imported
-#if __name__ == "__main__":
-#        main()
